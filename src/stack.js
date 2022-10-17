d68f08bb418739d78beb6cffb3e2f065dfebe07e
Lada <100614341+ladaln@users.noreply.github.com>
2022-10-17 09:01:13 +0300

    add: ready

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _arr = []
+
+  push(element) {
+    this._arr = this._arr.concat(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this._arr.length == 0) return undefined
+    let res = this._arr[this._arr.length - 1]
+    this._arr = this._arr.slice(0, this._arr.length - 1)
+    return res
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this._arr.length == 0) return undefined
+    return this._arr[this._arr.length - 1]
   }
 }
 
